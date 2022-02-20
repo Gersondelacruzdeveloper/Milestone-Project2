c290c7df46bbb10d66aef9cab7af3bf47a6b196b
Gerson <qapuntacana@gmail.com>
2022-02-20 21:49:34 +0000

    Add comments to the functions in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
-
+//variables
 let harryAnimation = document.getElementById('flying-animation')
 let home = document.getElementById('home')
 let header = document.getElementById('header')
 let card = document.getElementById('card')
 
 /**
- * This funtion display header element
- * and display none the main content from index.html
+ * this fuction start the Game 
  */
-
  function startGame(){
     toggle()
 }
+
+
+
+/**
+ * This funtion display header element
+ * and display none the main content from index.html
+ */
 
  function toggle(){
     home.style.display = "none"
