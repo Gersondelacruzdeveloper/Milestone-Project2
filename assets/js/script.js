91a30116cf780f13f467b8ab422855920547a2e6
Gerson <qapuntacana@gmail.com>
2022-03-01 20:56:54 +0000

    remove memorycard & change id card to section-card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //variables
-let harryAnimation = document.getElementById('flying-animation')
-let home = document.getElementById('home')
-let header = document.getElementById('header')
-let card = document.getElementById('card')
+let harryAnimation = document.getElementById('flying-animation');
+let home = document.getElementById('home');
+let header = document.getElementById('header');
+let card = document.getElementById('section-card');
 
 /**
  * this fuction start the Game 
@@ -11,17 +11,13 @@ let card = document.getElementById('card')
     toggleVisibility()
 }
 
-
-
-/**
+ /**
  * This funtion display header element
  * and display none the main content from index.html
  */
-
-function toggleVisibility(){
+  function toggleVisibility(){
     home.style.display = "none"
     harryAnimation.style.display = "none"
     header.style.display = "block"
     card.style.display = "block"
 }
-
