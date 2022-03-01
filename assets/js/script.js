41ccfce21c36e53c23b0def4bc0bfbb20d1d923b
Gerson <qapuntacana@gmail.com>
2022-03-01 21:33:45 +0000

    Add card list imformation

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,19 @@ let card = document.getElementById('section-card');
     header.style.display = "block"
     card.style.display = "block"
 }
+
+// Card list Information, include name and images
+const CardList = [
+  {name:"bellatrix", image:"bellatrix.png"},
+  {name:"bumbledore", image:"bumbledore.png"},
+  {name:"hagrid", image:"hagrid.png"},
+  {name:"harry", image:"harry.png"},
+  {name:"hermione", image:"hermione.png"},
+  {name:"luna", image:"luna.png"},
+  {name:"malfoy", image:"malfoy.png"},
+  {name:"mcGonagall", image:"mcGonagall.png"},
+  {name:"neville", image:"neville.png"},
+  {name:"ron", image:"ron.png"},
+  {name:"snape", image:"snape.png"},
+  {name:"voldermore", image:"voldermore.png"},
+];
